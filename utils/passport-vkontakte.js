3597a99a2ac5967f71a3a783e1057e7f6d2a8e77
AndreiSalnikov <Ans762@mail.ru>
2023-05-24 14:02:33 +0300

    feat: тесты авторизации

--- utils/passport-vkontakte.js
+++ utils/passport-vkontakte.js
@@ -5,34 +5,29 @@ const vkClientID = '51646210';
 const vkClientSecret = 'HrhQ2Omv7qVBIgYELFvg';
 const vkRedirectURI = 'https://wish-lister.ru/api/user/auth/vk/callback';
 
-try {
-  passport.use(
-    new VKontakteStrategy(
-      {
-        clientID: vkClientID,
-        clientSecret: vkClientSecret,
-        callbackURL: vkRedirectURI,
-        lang: 'ru',
-      },
-      (accessToken, refreshToken, params, profile, done) => {
-        // Here, you can access the user's VK.com profile information
-        // and perform any necessary operations like user authentication or account creation
-        // 'profile' object contains the user's VK.com profile data
-        // Example:
-        const user = {
-          vkontakteId: profile.id,
-          displayName: profile.displayName,
-          // ... add more user data as needed
-        };
-        // Call the 'done' callback to complete the authentication process
-        // Pass 'null' as the first argument to indicate no error, and 'user'
-        // as the second argument to provide the
-        // authenticated user object
-        done(null, user);
-      },
-    ),
-  )
-} catch (error) {
-  console.log(error)
-}
-
+passport.use(
+  new VKontakteStrategy(
+    {
+      clientID: vkClientID,
+      clientSecret: vkClientSecret,
+      callbackURL: vkRedirectURI,
+      lang: 'ru',
+    },
+    (accessToken, refreshToken, params, profile, done) => {
+      // Here, you can access the user's VK.com profile information
+      // and perform any necessary operations like user authentication or account creation
+      // 'profile' object contains the user's VK.com profile data
+      // Example:
+      const user = {
+        vkontakteId: profile.id,
+        displayName: profile.displayName,
+        // ... add more user data as needed
+      };
+      // Call the 'done' callback to complete the authentication process
+      // Pass 'null' as the first argument to indicate no error, and 'user'
+      // as the second argument to provide the
+      // authenticated user object
+      done(null, user);
+    },
+  ),
+);
