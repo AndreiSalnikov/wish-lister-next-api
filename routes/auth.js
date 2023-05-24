bebcb89d78c66409ef54dc5d74e1d45fba218228
AndreiSalnikov <Ans762@mail.ru>
2023-05-24 14:00:54 +0300

    feat: тесты авторизации

--- routes/auth.js
+++ routes/auth.js
@@ -9,18 +9,10 @@ router.get('/user/auth/vk', passport.authenticate('vkontakte', {
   scope: ['email', 'friends'],
 }));
 
-router.get('/user/auth/vk/callback',
-  passport.authenticate('vkontakte',
-    {
-      successRedirect: '/lists',
-      failureRedirect: '/',
-    },
-(err) => {
-      if (err) {
-        console.error(err)
-      }
-    },
-));
+router.get('/user/auth/vk/callback', passport.authenticate('vkontakte', {
+  successRedirect: '/lists',
+  failureRedirect: '/',
+}));
 
 /*
 router.get('/auth/mailru', passport.authenticate('mailru'));
